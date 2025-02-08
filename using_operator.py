a2a153c7fd2bf89902e1079a4883621fafa57df2
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-08 11:26:31 +0600

    Add files via upload

---
+++ using_operator.py
@@ -0,0 +1,13 @@
+#using operator
+     
+   
+num = int(input("Enter a value: "))
+
+add = num + 5
+
+# Output
+print("The sum is %d" %add)
+x=int(input("enter the value is"))
+sum=x+77
+print("sum is %d" %sum)
+x,y=input("Enter input number").split()
