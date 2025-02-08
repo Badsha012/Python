48ca8418561a2127e15867823d237255d48e5060
Badsha Sheih <badshasheikh361@gmail.com>
2025-02-08 11:27:17 +0600

    Add files via upload

---
+++ printing_variable.py
@@ -0,0 +1,32 @@
+#printing variable using pythjon programming
+#single variable
+
+s="Bob"
+print(s)
+#multiple variable
+
+a="Badsha sheilh"
+age=23
+city="khulna city"
+print(a,age,city)
+
+
+#out formating
+
+amount =159.77
+print("Amount= ${:.2f}".format(amount))
+
+
+#using step parameter
+print("Python",end='@')
+print("Developer")
+
+#separing with comma
+
+print('C','S','E',sep='')
+#for formating with date
+print('05','02','2025',sep='-')
+
+
+#another example
+print('badshash','sheikh2022',sep='@')
