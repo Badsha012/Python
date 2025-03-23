548bf883f7725be6c48160cf8de865a2f2d0435a
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-23 10:08:28 +0600

    Add files via upload

---
+++ if_elif_else_statemet.py
@@ -0,0 +1,14 @@
+#if coditional:
+#code
+#elif conditional:
+#code
+#else:
+#code
+#Example:
+a=int(input("Enter a number:"))
+if a>0:
+    print(f'{a} is a positive number.')
+elif a==0:
+    print(f'{a} is zero.')
+else:
+    print(f'{a} is a negative number.')
