073ea027931b692fdca2ef67999651df3dceeedd
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-23 09:59:33 +0600

    Add files via upload

---
+++ if_else_statement.py
@@ -0,0 +1,10 @@
+#if condition:
+#statement
+#else:
+#statement
+# Example 1
+a=int(input("Enter a number:"))
+if a>0:
+    print(f'{a} is a positive number.')
+else:
+    print(f'{a} is a negative number.')
