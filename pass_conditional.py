c414875ede6d31e040c0bec17c95cbc1f5412334
Badsha Sheih <badshasheikh361@gmail.com>
2025-03-25 09:24:33 +0600

    Add files via upload

---
+++ pass_conditional.py
@@ -0,0 +1,8 @@
+#using pass conditional statement
+
+for i in range(5):
+    if i==3:
+        pass
+    print(i)
+    #output: 0 1 2 3 4
+    #pass statement does nothing, it only acts as a placeholder
